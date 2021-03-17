b38331d005353d2a75b96c04eb1aef263ddab302
Spandita Banerjee <spanditabanerjee@gmail.com>
2021-03-17 09:05:08 +0530

    first commit

---
+++ src/components/UI/card/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+/**
+ * @author
+ * @function Card
+ **/
+
+const Card = (props) => {
+    return ( <
+        div className = "card" > { props.children } <
+        /div>
+    )
+
+}
+
+export default Card
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import firebase from 'firebase';
+import { Provider } from 'react-redux';
+import store from './store';
 
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyA8CO0Nu14yloCRUTMbwBqTP25xFqos6vQ",
     authDomain: "web-messenger-80be7.firebaseapp.com",
@@ -14,18 +18,25 @@ const firebaseConfig = {
     appId: "1:1024095015994:web:0497cd4f83d69f278e9103",
     measurementId: "G-R0SKJNK9WX"
 };
+
+
 firebase.initializeApp(firebaseConfig);
 
+window.store = store;
+
 ReactDOM.render( <
+    Provider store = { store } >
+    <
     React.StrictMode >
     <
     App / >
     <
-    /React.StrictMode>,
+    /React.StrictMode> < /
+    Provider > ,
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

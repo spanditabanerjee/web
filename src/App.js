e564cabd2de1365bff20050ed5018fa9a21eb958
Spandita Banerjee <spanditabanerjee@gmail.com>
2021-03-17 07:42:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './containers/HomePage';
+import LoginPage from './containers/LoginPage';
+import RegisterPage from './containers/RegisterPage';
+import PrivateRoute from './components/PrivateRoute';
+import { useDispatch, useSelector } from 'react-redux';
+import { isLoggedInUser } from './actions';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const auth = useSelector(state => state.auth);
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        if (!auth.authenticated) {
+            dispatch(isLoggedInUser())
+        }
+    }, []);
+
+
+    return ( <
+        div className = "App" >
+        <
+        Router > { /* only logged in user can access this home route */ } <
+        PrivateRoute path = "/"
+        exact component = { HomePage }
+        />
+
+
+        <
+        Route path = "/login"
+        component = { LoginPage }
+        /> <
+        Route path = "/signup"
+        component = { RegisterPage }
+        /> <
+        /Router> <
+        /div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const firebaseConfig = {
+    apiKey: "AIzaSyA8CO0Nu14yloCRUTMbwBqTP25xFqos6vQ",
+    authDomain: "web-messenger-80be7.firebaseapp.com",
+    projectId: "web-messenger-80be7",
+    storageBucket: "web-messenger-80be7.appspot.com",
+    messagingSenderId: "1024095015994",
+    appId: "1:1024095015994:web:0497cd4f83d69f278e9103",
+    measurementId: "G-R0SKJNK9WX"
+};
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
